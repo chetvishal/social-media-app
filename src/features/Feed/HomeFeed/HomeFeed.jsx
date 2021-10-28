483b1698eaf60b239617d3710cafcf6c7b98efc7
chetvishal <vishal1982c@gmail.com>
2021-10-28 12:27:37 +0530

    Feat: if no feed is empty its showing user suggestions

--- src/features/Feed/HomeFeed/HomeFeed.jsx
+++ src/features/Feed/HomeFeed/HomeFeed.jsx
@@ -1,8 +1,11 @@
 import { PostCard, Loader } from '../../../Components';
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { loadFeed } from '../feedSlice';
 import { checkLikedPost } from '../../../Services/PostServices';
+import axios from 'axios';
+import { apiEndPoint } from '../../../Services/Api';
+import { FollowerCard } from '../../Profile/Followers/Followers';
 
 export const HomeFeed = () => {
 
@@ -14,12 +17,27 @@ export const HomeFeed = () => {
         return state.auth
     })
 
+    const [suggestions, setSuggestions] = useState([])
+
+    const suggestUsers = async () => {
+        const response = await axios.get(`${apiEndPoint()}/user/suggestions/${userId}`, {
+
+            headers: {
+                'Authorization': token
+            }
+        })
+        return response.data.suggestions;
+    }
+
     const dispatch = useDispatch()
 
     useEffect(() => {
         (
             async function () {
                 await dispatch(loadFeed({ userId, token }))
+                const suggestions = await suggestUsers();
+                setSuggestions(() => suggestions)
+                console.log("suggestions: ", suggestions);
             }
         )()
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -32,7 +50,7 @@ export const HomeFeed = () => {
                     <div><Loader /></div> :
                     status === "error" ?
                         <div>error {error}</div> :
-                        feed.map(item => {
+                        feed.length !== 0 ? feed.map(item => {
                             return <PostCard
                                 content={item.content}
                                 name={item.userId.name}
@@ -44,7 +62,13 @@ export const HomeFeed = () => {
                                 avatarUrl={item?.userId?.avatarUrl}
                                 key={item?._id}
                             />
-                        })
+                        }) : <div> 
+                            <h3>Users to follow</h3>
+                        {
+                            suggestions.map(item => {
+                                return <FollowerCard name={item.name} username={item.username} avatarUrl={item?.avatarUrl} />
+                            })
+                        }</div>
             }
         </div>
     )
