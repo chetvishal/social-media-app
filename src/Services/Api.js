2461c93b492f41f760080c9577981e6276fe78e3
chetvishal <vishal1982c@gmail.com>
2021-11-27 11:31:31 +0530

    Fix: changed to working api

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,3 +1,4 @@
 export const apiEndPoint = () => {
-    return `https://social-street-api.herokuapp.com`
+    // return `https://social-street-api.herokuapp.com`
+    return `https://social-media-backend.vishal1982c.repl.co`
 }
