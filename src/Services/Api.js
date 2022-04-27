be18695d7451cea1209b80ec23be4a8a6190b335
chetvishal <vishal1982c@gmail.com>
2022-04-27 22:44:08 +0530

    root api change

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,4 +1,4 @@
 export const apiEndPoint = () => {
-    // return `https://social-street-api.herokuapp.com`
-    return `https://social-media-backend.vishal1982c.repl.co`
+    return `https://social-street-api.herokuapp.com`
+    // return `https://social-media-backend.vishal1982c.repl.co`
 }
